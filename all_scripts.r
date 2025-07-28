79ac8e16f54b26871ef97d44ac9cdfa2b7e348ba
miguelestradam36 <miguelestradam36@gmail.com>
2025-07-28 15:59:04 -0600

    Regressions with R

--- all_scripts.r
+++ all_scripts.r
@@ -44,3 +44,76 @@ glimpse(house_sales)
 
 ## Create a broad overview of a data set
 skim(house_sales)
+
+## Create a simple linear regression model using the variables
+simple_model <- lm(volume ~ sales, data=txhousing)
+simple_model
+
+## Plot the regression line for the model
+ggplot(txhousing, aes(x = sales, y = volume)) +
+  geom_point() +
+  stat_smooth(method = lm) +
+  labs(title = "Regression of Sales on Volume of Housing sales in TX",
+       x = "Sales", y = "Volume")
+
+## Plotting the fitted model
+plot(simple_model)
+
+## Assess the summary of the fitted model
+summary(simple_model)
+
+## Calculate the confidence interval for the coefficients
+confint(simple_model)
+
+## Build a log transformed regression model
+model_transformed <- lm(log10(volume) ~ sales, data=txhousing)
+
+## Return the summary of the model
+summary(model_transformed)
+
+## Return the first diagnostic plot for the model
+plot(model_transformed, 1)
+
+## Return the second diagnostic plot for the model
+plot(model_transformed, 2)
+
+## Return the third diagnostic plot for the model
+plot(model_transformed, 3)
+
+## Find the fitted values of the simple regression model
+fitted_values <- predict.lm(simple_model)
+head(fitted_values, 3)
+
+## Return the model metrics
+model_metrics <- augment(simple_model)
+model_metrics
+
+## Predict new values using the model
+predict(simple_model, newdata= data.frame(sales = c(210, 27, 140)))
+
+## Build the multiple regression model with volume as the y variable and sales, median and age on the x variables
+multiple_reg <- lm(volume ~ sales + median, data=txhousing)
+
+## This prints the result of the model
+multiple_reg
+
+## Check the summary of the multiple regression model
+summary(multiple_reg)
+
+## Plot the fitted multiple regression model
+autoplot(multiple_reg)
+
+## Create a broad overview of the data set
+skim(house_sales)
+
+## Drop the missing values in the LotFrontage variable
+house_sales <- house_sales %>% drop_na(LotFrontage)
+
+## Build the multiple regression model 
+house_reg <- lm(SalePrice  ~ .,data=house_sales)
+
+## Check the summary of the multiple regression model
+summary(house_reg)
+
+## Perform diagnostic plots of the fitted multiple regression model
+autoplot(house_reg)
